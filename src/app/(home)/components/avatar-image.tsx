95adae007c8de190fa98f2572dcc12846b0a0266
RahmadAmri <rahmadamri3@gmail.com>
2025-09-14 23:46:34 +0700

    Feat: remove jpg to png

--- src/app/(home)/components/avatar-image.tsx
+++ src/app/(home)/components/avatar-image.tsx
@@ -1,10 +1,12 @@
+"use client";
+
 import Image from "next/image";
 
 export function AvatarImage() {
   return (
     <div className="relative h-10 w-10 sm:h-20 sm:w-20 overflow-hidden rounded-xl ring-1">
       <Image
-        src="/assets/amri.jpg"
+        src="/assets/amri.png"
         alt="Rahmad Amri"
         fill
         priority
